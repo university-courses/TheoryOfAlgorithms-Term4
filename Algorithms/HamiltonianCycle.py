3a4e661be70f572083c437d7ecf2c5060f7c3269
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-05-11 00:46:20 +0300

    updated HamiltonianCycle class, added 'find_shortest_path()' method into HamiltonianCycle class

--- Algorithms/HamiltonianCycle.py
+++ Algorithms/HamiltonianCycle.py
@@ -1,28 +1,46 @@
 class HamiltonianCycle:
 
-	def find_path(self, graph, start, end, path=[]):
+	def __init__(self, graph):
+		self.__graph = graph
+
+	def find_path(self, start, end, path=[]):
 		path = path + [start]
 		if start == end:
 			return path
-		if start not in graph.keys():
+		if start not in self.__graph.keys():
 			return None
-		for node in graph[start]:
+		for node in self.__graph[start]:
 			if node not in path:
-				new_path = self.find_path(graph, node, end, path)
+				new_path = self.find_path(node, end, path)
 				if new_path:
 					return new_path
 		return None
 
-	def find_all_paths(self, graph, start, end, path=[]):
+	def find_all_paths(self, start, end, path=[]):
 		path = path + [start]
 		if start == end:
 			return [path]
-		if start not in graph.keys():
+		if start not in self.__graph.keys():
 			return []
 		paths = []
-		for node in graph[start]:
+		for node in self.__graph[start]:
 			if node not in path:
-				new_paths = self.find_all_paths(graph, node, end, path)
+				new_paths = self.find_all_paths(node, end, path)
 				for new_path in new_paths:
 					paths.append(new_path)
 		return paths
+
+	def find_shortest_path(self, start, end, path=[]):
+		path = path + [start]
+		if start == end:
+			return path
+		if start not in self.__graph.keys():
+			return None
+		shortest = None
+		for node in self.__graph[start]:
+			if node not in path:
+				new_path = self.find_shortest_path(node, end, path)
+				if new_path:
+					if not shortest or len(new_path) < len(shortest):
+						shortest = new_path
+		return shortest
--- Tests/Unittest/HamiltonianCycleTest.py
+++ Tests/Unittest/HamiltonianCycleTest.py
@@ -5,23 +5,25 @@ from Algorithms.HamiltonianCycle import HamiltonianCycle
 
 class TestHamiltonianCycle(TestCase):
 
-	graph = {
+	h_cycle = HamiltonianCycle({
 		1: [4, 5],
 		2: [5],
 		3: [4],
 		4: [1, 3, 5],
 		5: [1, 2, 4]
-	}
+	})
 
 	def test_find_path(self):
-		h_cycle = HamiltonianCycle()
-		self.assertEqual(h_cycle.find_path(self.graph, 1, 5), [1, 4, 5])
+		self.assertEqual(self.h_cycle.find_path(1, 5), [1, 4, 5])
 
 	def test_find_all_paths(self):
-		h_cycle = HamiltonianCycle()
-		self.assertEqual(h_cycle.find_all_paths(self.graph, 1, 5), [[1, 4, 5], [1, 5]])
+		self.assertEqual(self.h_cycle.find_all_paths(1, 5), [[1, 4, 5], [1, 5]])
+
+	def test_find_shortest_path(self):
+		self.assertEqual(self.h_cycle.find_shortest_path(1, 5), [1, 5])
 
 
 def run(suite):
 	suite.addTest(TestHamiltonianCycle('test_find_path'))
 	suite.addTest(TestHamiltonianCycle('test_find_all_paths'))
+	suite.addTest(TestHamiltonianCycle('test_find_shortest_path'))
